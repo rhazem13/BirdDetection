d0f85946ead72cffd8b2d056c709627e9135fdf2
Hazem Ragab <63895193+rhazem13@users.noreply.github.com>
2022-11-05 18:26:15 +0200

    Update query_engine.py

--- query_engine.py
+++ query_engine.py
@@ -1,19 +1,21 @@
 
 
 from sql_metadata import Parser
-from sql_metadata.compat import get_query_columns
+from sql_metadata.compat import get_query_columns, get_query_tables
 import pandas as pd
 
 #def execute(query) : 
 
-_parse("SELECT COUNT(*) as cnt FROM dbo.main_table WHERE (VKRKA10001920015 = 1 AND age IN (15, 16, 17, 18, 19) AND income IN (101, 102, 301, 302, 305))")
 """
     takes query as string
     returns: dictionary holds table name, and attributes
 """
-def _parse(query): 
-    print(get_query_columns(query))
+def _parse(query):
+    result = {'Tables: ':get_query_tables(query),'Columns':get_query_columns(query)} 
+    return result
 #-> dict{table, cols}
+#test
+#_parse("SELECT COUNT(*) as cnt FROM dbo.main_table WHERE (VKRKA10001920015 = 1 AND age IN (15, 16, 17, 18, 19) AND income IN (101, 102, 301, 302, 305))")
 """
     takes filename (string), and attributes(list)
     returns: list of rows
