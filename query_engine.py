4bb158e2bfe51a6b790bc2e4009479f966771a28
Mo'men Ashraf Muhamed <79394414+Moamen189@users.noreply.github.com>
2022-11-05 18:40:43 +0200

    Update query_engine.py

--- query_engine.py
+++ query_engine.py
@@ -2,17 +2,25 @@
 
 
 
+
+
+
+
 from sql_metadata import Parser
 from sql_metadata.compat import get_query_columns, get_query_tables
 import pandas as pd
 
 def execute(query) : 
+    
     dic = _parse(query)
-   
+   ## Take first Table Only 
     DF =  _extract_attrs_from_file(dic["Tables"] , dic["Columns"])
 
     return DF 
 
+
+###function execute check table long than 1 read exception
+### else  DF =  _extract_attrs_from_file(dic["Tables"][0] , dic["Columns"])
 """
     takes query as string
     returns: dictionary holds table name, and attributes
@@ -31,4 +39,6 @@ def _extract_attrs_from_file(filename, attrs):
     else:
         df = pd.read_csv(filename, usecols=attrs)
     return df
+    
+
     
